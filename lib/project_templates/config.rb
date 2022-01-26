961f335c97e66ae49c0cd117eb60c08557de0ecd
Cute HaX0r <cutehaX0r@github.com>
2022-01-25 21:58:58 -0700

    Build path attributes for config
    This adds the most important paths as attributes for config.
    
    * `template_path` is the location of project templates
      By default imagine something like ~/.local/share/project_templates
    
    * `project_path` is the location of the specific project template to be
      used as a source. By default imagine something like
      ~/.local/share/project/templates/ruby_application
    
      in this example `project_name` would be `ruby_application` and it was
      appended to `template_path` to compute this path.
    
    * `target_path` is the destination where the template will be generated.
      by default this would be a path in the current directory plus
      `target_name`. Something like ~/src/github.com/me/cool_app if
      `target_name` was "cool_app" and the current working directory was
      ~/src/github.com/me/
    
    It also defines a few DEFAULT_ constants. These might have to go and we'll just
    make everything "mandatory". I'll figure that out once all the
    attributes are in place.
    
    Finally, there's some minor test clean up to use the new helpers and to
    simplify initialization: one for "it uses defaults" and one for "it uses
    values passed in".

--- lib/project_templates/config.rb
+++ lib/project_templates/config.rb
@@ -12,21 +12,49 @@ module ProjectTemplates
   class Config
     extend T::Sig
 
+    DEFAULT_TEMPLATE_PATH = T.let(Pathname.new("~").expand_path, Pathname)
+    DEFAULT_PROJECT_PATH = T.let(Pathname.new("~").expand_path, Pathname)
+    DEFAULT_TARGET_PATH = T.let(Pathname.new(Dir.pwd).expand_path, Pathname)
+
     sig { returns(T::Boolean) }
     # When dry-run is true no changes to the file system will be made but
     # all other steps will be completed. Access to the target path will be
     # verified and all other compilation steps will complete.
     attr_accessor :dry_run
 
-    # attr_reader :user_path, :project_path, :template_path, :target_path, :template_name, :target_name,
-    #             :user_variables, :project_variables
+    sig { returns(Pathname) }
+    # The path where template projects are located. This is the path where
+    # the project_path is expected to be
+    attr_accessor :template_path
 
-    sig { params(dry_run: T::Boolean).void }
+    sig { returns(Pathname) }
+    # The path which will be used as the "source" for creating the new project
+    # by default this would be located within #template_path and is computed by
+    # joining project_name to template_path.
+    attr_accessor :project_path
+
+    sig { returns(Pathname) }
+    # The path which will be created by copying files from the project_path to
+    # this location. The is computed by appending target_name to the current
+    # working directory
+    attr_accessor :target_path
+
+    # TODO: attr_accessor :template_name, :target_name, :user_variables, :project_variables
+
+    sig { params(dry_run: T::Boolean, template_path: Pathname, project_path: Pathname, target_path: Pathname).void }
     # Initialize a config by explicitly setting all of the values. If you
     # don't want to set all values consider using one of the class methods
     # instead.
-    def initialize(dry_run: false)
+    def initialize(
+      dry_run: false,
+      template_path: DEFAULT_TEMPLATE_PATH,
+      project_path: DEFAULT_PROJECT_PATH,
+      target_path: DEFAULT_TARGET_PATH
+    )
       @dry_run = dry_run
+      @template_path = T.let(template_path, Pathname)
+      @project_path = T.let(project_path, Pathname)
+      @target_path = T.let(target_path, Pathname)
     end
 
     alias dry_run? dry_run
--- test/project_templates/test_config.rb
+++ test/project_templates/test_config.rb
@@ -3,23 +3,43 @@
 require "test_helper"
 
 class TestConfig < MiniTest::Test
-  attr_reader :config
+  extend HasAttributeHelper
+
+  attr_reader :config, :class_under_test
 
   def setup
-    @config = ProjectTemplates::Config.new
+    @class_under_test = ProjectTemplates::Config
+    @config = class_under_test.new
   end
 
-  def test_initializes_dry_run_to_false_by_default
-    refute config.dry_run?
+  test_has_attribute(:config, :template_path, readable: true, writable: true)
+  test_has_attribute(:config, :project_path, readable: true, writable: true)
+  test_has_attribute(:config, :target_path, readable: true, writable: true)
+  test_has_attribute(:config, :dry_run, readable: true, writable: true, interrogatable: true)
+
+  def test_defines_default_constants
+    assert_equal(Pathname.new("~").expand_path, class_under_test::DEFAULT_TEMPLATE_PATH)
+    assert_equal(Pathname.new("~").expand_path, class_under_test::DEFAULT_PROJECT_PATH)
+    assert_equal(Pathname.new(Dir.pwd).expand_path, class_under_test::DEFAULT_TARGET_PATH)
   end
 
-  def test_it_initializes_dry_run
-    config = ProjectTemplates::Config.new(dry_run: true)
-    assert config.dry_run?
+  def test_initialize_uses_defaults
+    refute(config.dry_run?)
+    assert_equal(class_under_test::DEFAULT_TEMPLATE_PATH, config.template_path)
+    assert_equal(class_under_test::DEFAULT_PROJECT_PATH, config.project_path)
+    assert_equal(class_under_test::DEFAULT_TARGET_PATH, config.target_path)
   end
 
-  def test_has_a_dry_run_method
-    assert_respond_to config, :dry_run?
-    assert_respond_to config, :dry_run
+  def test_initialize_can_be_provided_all_arguments
+    args = {
+      dry_run: true,
+      template_path: Pathname.new("/").expand_path,
+      project_path: Pathname.new("/").expand_path,
+      target_path: Pathname.new("/").expand_path,
+    }
+    inited_config = class_under_test.new(**args)
+    args.each do |argument, expected_value|
+      assert_equal expected_value, inited_config.send(argument)
+    end
   end
 end
